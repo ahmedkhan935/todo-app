6a11e7ab4ec9e4647f0c059e0d9e2fdc52dbf1e4
ahmedkhan935 <ahmedkhanraza935@gmail.com>
2025-01-07 23:47:53 +0500

    add Jest configuration and setup files for testing

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+// jest.config.js
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/lib/(.*)$': '<rootDir>/lib/$1',
+  }
+}
+
+module.exports = createJestConfig(customJestConfig)
